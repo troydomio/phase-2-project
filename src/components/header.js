e2fba5fc8da28542fd1bdecac66cb93203241256
Troy <troyakins99@yahoo.com>
2021-08-02 14:00:36 -0500

    Added logo and header text to header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,18 @@
 
 
 function Header() {
+    const logo = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvvpheaMLN23qpDsoaOFAo9oT6Qp3m2LIcAg&usqp=CAU"
     return (
-        <div className="App-header">
-            Header
-        </div>
+        <header>
+           <img src={logo} style={{width:"170px", height:"190px", paddingLeft:"250px", }}/>
+           <p style={{textAlign:"center",height: "25px", paddingTop:"1px", paddingRight:"100px", fontSize:"50px"}}>Things To Do In Texas</p>
+        </header>
+
+        // height: 100px; 
+        // display: block; 
     )
 }
 
 export default Header
+
+{/* <div className="App-header"> */}
